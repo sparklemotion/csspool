76753193f95b0208c370aa411ea575dc85aad386
jbarnette <jbarnette@9d21fa00-d5df-4491-9186-54a82f31e458>
2007-09-19 02:00:02 +0000

    Factored our @sac.
    git-svn-id: svn://rubyforge.org/var/svn/csspool/trunk@2 9d21fa00-d5df-4491-9186-54a82f31e458

--- test/test_sac.rb
+++ test/test_sac.rb
@@ -2,52 +2,55 @@ require 'test/unit'
 require 'css/sac'
 
 class SACParserTest < Test::Unit::TestCase
+  def setup
+    @sac = CSS::SAC.new()
+  end
+  
   def test_parse_simple
-    sac = CSS::SAC.new()
-      sac.parse('
-        @import "subs.css";
-        * {  margin: 0px; }
-        body {
-          margin: 0px;
-          padding: 0px;
-        }
-      ')
-      assert_equal(39, sac.tokens.length)
+    @sac.parse('
+      @import "subs.css";
+      * {  margin: 0px; }
+      body {
+        margin: 0px;
+        padding: 0px;
+      }
+    ')
+
+    assert_equal(39, @sac.tokens.length)
   end
 
   def test_at_import
-    sac = CSS::SAC.new()
-    sac.parse('@import "subs.css" print;')
-    assert_equal(6, sac.tokens.length)
+    @sac.parse('@import "subs.css" print;')
+    assert_equal(6, @sac.tokens.length)
   end
 
   def test_at_media
-    sac = CSS::SAC.new()
-    sac.parse('@media print { h1 { color: black; } }')
-    assert_equal(19, sac.tokens.length)
+    @sac.parse('@media print { h1 { color: black; } }')
+    assert_equal(19, @sac.tokens.length)
   end
 
   def test_at_page
-    sac = CSS::SAC.new()
-    sac.parse('@page print { color: black; }')
-    assert_equal(13, sac.tokens.length)
+    @sac.parse('@page print { color: black; }')
+    assert_equal(13, @sac.tokens.length)
 
-    sac.parse('@page :left { color: black; }')
-    assert_equal(14, sac.tokens.length)
+    @sac.parse('@page :left { color: black; }')
+    assert_equal(14, @sac.tokens.length)
 
-    sac.parse('@page print :left { color: black; }')
-    assert_equal(16, sac.tokens.length)
+    @sac.parse('@page print :left { color: black; }')
+    assert_equal(16, @sac.tokens.length)
   end
 
   def test_at_font_face
-    sac = CSS::SAC.new()
-    sac.parse('@font-face { color: black; }')
-    assert_equal(11, sac.tokens.length)
+    @sac.parse('@font-face { color: black; }')
+    assert_equal(11, @sac.tokens.length)
   end
 
   def test_ignorable_at
-    sac = CSS::SAC.new()
-    sac.parse('@aaron { color: black; }')
-    assert_equal(11, sac.tokens.length)
+    @sac.parse('@aaron { color: black; }')
+    assert_equal(11, @sac.tokens.length)
+  end
+  
+  def test_something
+    
   end
 end
