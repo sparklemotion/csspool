5f9e19b830efb047c2b7e47a2888bbce25bd9f7d
aaronp <aaronp@9d21fa00-d5df-4491-9186-54a82f31e458>
2007-11-27 06:20:41 +0000

    updating version
    git-svn-id: svn://rubyforge.org/var/svn/csspool/trunk@148 9d21fa00-d5df-4491-9186-54a82f31e458

--- lib/css/sac/parser.rb
+++ lib/css/sac/parser.rb
@@ -14,7 +14,7 @@ module CSS
   module SAC
     class Parser < CSS::SAC::GeneratedParser
       # The version of CSSPool you're using
-      VERSION = '0.1.1'
+      VERSION = '0.2.0'
 
       TOKENIZER = Tokenizer.new
       
