f50210342f4c59b229201cab1ac04e841ed87c5c
Aaron Patterson <aaron.patterson@gmail.com>
2009-06-10 16:20:11 -0700

    start document, end_document, and charset implemented

--- Rakefile
+++ Rakefile
@@ -3,16 +3,9 @@
 require 'rubygems'
 require 'hoe'
 require './lib/crocodile/version.rb'
-gem 'rake-compiler', '>= 0.4.1'
-require "rake/extensiontask"
 
 HOE = Hoe.new('crocodile', Crocodile::VERSION) do |p|
   p.developer('Aaron Patterson', 'aaronp@rubyforge.org')
 end
 
-Rake::ExtensionTask.new("crocodile", HOE.spec) do |ext|
-  ext.lib_dir = "lib/crocodile"
-end
-
-Rake::Task[:test].prerequisites << :compile
 # vim: syntax=Ruby
--- lib/crocodile/sac/parser.rb
+++ lib/crocodile/sac/parser.rb
@@ -20,6 +20,36 @@ module Crocodile
       def pop
         @selector_stack.pop
       end
+
+      def parse_memory string, encoding
+        parser = LibCroco::Parser.new(
+          LibCroco.cr_parser_new_from_buf(
+            string,
+            string.length,
+            5,
+            0
+          )
+        )
+        sac_handler =
+          LibCroco::CSSSACHandler.new(LibCroco.cr_doc_handler_new)
+
+        sac_handler[:start_document] = lambda { |parser|
+          @document.start_document
+        }
+        sac_handler[:end_document] = lambda { |parser|
+          @document.end_document
+        }
+        sac_handler[:charset] = lambda { |dh, name, location|
+          @document.charset(
+            LibCroco.cr_string_peek_raw_str(name).read_string,
+            LibCroco::CRParsingLocation.new(location).to_h
+          )
+        }
+
+        LibCroco.cr_parser_set_sac_handler(parser, sac_handler)
+        LibCroco.cr_parser_parse(parser)
+        LibCroco.cr_doc_handler_destroy(sac_handler)
+      end
     end
   end
 end
