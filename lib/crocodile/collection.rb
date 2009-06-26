1b850a605830a857673cf380f30045bf3687a757
Aaron Patterson <aaron.patterson@gmail.com>
2009-06-26 14:54:04 -0700

    testing recursion and collection import

--- lib/crocodile/collection.rb
+++ lib/crocodile/collection.rb
@@ -7,15 +7,36 @@ module Crocodile
 
     def << string
       doc = Crocodile.CSS string
-      doc.import_rules.each do |ir|
-        @docs << ir.load(&@block)
+
+      import_tree = [[doc]]
+
+      imported_urls = {}
+
+      until import_tree.last.all? { |x| x.import_rules.length == 0 }
+        level = import_tree.last
+        import_tree << []
+        level.each do |doc|
+          doc.import_rules.each do |ir|
+            next if imported_urls.key? ir.uri
+
+            new_doc = ir.load(&@block)
+
+            imported_urls[ir.uri] = ir.load(&@block)
+            import_tree.last << new_doc
+          end
+        end
       end
-      @docs << doc
+
+      @docs += import_tree.flatten
       self
     end
 
     def length
       @docs.length
+    end
+
+    def [] idx
+      @docs[idx]
     end
   end
 end
--- test/test_collection.rb
+++ test/test_collection.rb
@@ -25,5 +25,38 @@ module Crocodile
       assert called, "block was not called"
       assert_equal 2, collection.length
     end
+
+    def test_collection_imports_imports_imports
+      css = {
+        'foo.css' => '@import url("bar.css");',
+        'bar.css' => '@import url("baz.css");',
+        'baz.css' => 'div { background: red; }',
+      }
+
+      collection = Crocodile::Collection.new do |url|
+        css[url] || raise
+      end
+
+      collection << '@import url(foo.css);'
+      assert_equal 4, collection.length
+      assert_nil collection[0].parent
+      assert_equal collection[0], collection[1].parent
+      assert_equal collection[1], collection[2].parent
+      assert_equal collection[2], collection[3].parent
+    end
+
+    def test_load_only_once
+      css = {
+        'foo.css' => '@import url("foo.css");',
+      }
+
+      collection = Crocodile::Collection.new do |url|
+        css[url] || raise
+      end
+
+      collection << '@import url(foo.css);'
+
+      assert_equal 2, collection.length
+    end
   end
 end
