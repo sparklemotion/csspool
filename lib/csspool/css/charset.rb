1ba824b9ad309468cd9398664210f14db30b0219
Mike Tierney <mike@panpainter.com>
2011-08-16 19:01:16 -0700

    Changing Charset to a Node

--- lib/csspool/css/charset.rb
+++ lib/csspool/css/charset.rb
@@ -1,7 +1,12 @@
 module CSSPool
   module CSS
-    class Charset < Struct.new(:name, :parse_location)
-      include CSSPool::Visitable
+    class Charset < CSSPool::Node
+      attr_accessor :name, :parse_location
+
+      def initialize name, parse_location
+        @name = name
+        @parse_location = parse_location
+      end
     end
   end
 end
