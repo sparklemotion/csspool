0a4703d9931a3b7fddcd787f9ccda09533a3b91b
Mike Tierney <mike@panpainter.com>
2011-08-16 19:20:21 -0700

    Changing Declaration to a Node

--- lib/csspool/css/declaration.rb
+++ lib/csspool/css/declaration.rb
@@ -1,8 +1,19 @@
 module CSSPool
   module CSS
-    class Declaration < Struct.new(:property, :expressions, :important, :rule_set)
-      include CSSPool::Visitable
+    class Declaration < CSSPool::Node
+      attr_accessor :property
+      attr_accessor :expressions
+      attr_accessor :important
+      attr_accessor :rule_set
+
       alias :important? :important
+
+      def initialize property, expressions, important, rule_set
+        @property = property
+        @expressions = expressions
+        @important = important
+        @rule_set = rule_set
+      end
     end
   end
 end
